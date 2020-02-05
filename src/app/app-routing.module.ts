6f5aee9cb53968faa1eed3ef170ad3897acfa2df
Lex <lex.bay93@gmail.com>
2020-02-05 12:53:15 -0600

    studio work from class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GenresComponent } from './genres/genres.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "genres", component: GenresComponent },
+  { path: "artists", component: ArtistsComponent },
+  { path: "albums", component: AlbumsComponent },
+  { path: "home", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GenresComponent } from './genres/genres.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenresComponent,
+    ArtistsComponent,
+    AlbumsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
